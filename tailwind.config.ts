cff92d528aa84ce74008d96cd494c200e9ef3411
Ryan Kwan <klhong124@gmail.com>
2024-07-07 14:32:12 +0100

    bug fix

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,9 +1,16 @@
 import type { Config } from "tailwindcss";
+const svgToDataUri = require("mini-svg-data-uri");
+
+const colors = require("tailwindcss/colors");
+const {
+  default: flattenColorPalette,
+} = require("tailwindcss/lib/util/flattenColorPalette");
+
 
 const config: Config = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./ui/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
@@ -15,6 +22,19 @@ const config: Config = {
       },
     },
   },
-  plugins: [],
+  plugins: [
+    function ({ matchUtilities, theme }: any) {
+      matchUtilities(
+        {
+          "bg-dot": (value: any) => ({
+            backgroundImage: `url("${svgToDataUri(
+              `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="16" height="16" fill="none"><circle fill="${value}" id="pattern-circle" cx="10" cy="10" r="1.6257413380501518"></circle></svg>`
+            )}")`,
+          }),
+        },
+        { values: flattenColorPalette(theme("backgroundColor")), type: "color" }
+      );
+    },
+  ],
 };
 export default config;
---
+++ ui/flip-words.tsx
@@ -0,0 +1,107 @@
+"use client";
+import React, { useCallback, useEffect, useRef, useState } from "react";
+import { AnimatePresence, motion, LayoutGroup } from "framer-motion";
+import { cn } from "@/utils/cn";
+
+export default function FlipWords({
+  words = [
+    "Seamless Integration",
+    "AI-Driven Insights",
+    "Stunning Visuals",
+    "Cutting-Edge Technology",
+    "Elegant Animations",
+    "Modern Frameworks"
+  ],
+  duration = 3000,
+  className,
+}: Readonly<{
+  words?: readonly string[];
+  duration?: number;
+  className?: string;
+}>) {
+  const [currentWord, setCurrentWord] = useState(words[0]);
+  const [isAnimating, setIsAnimating] = useState<boolean>(false);
+
+  const startAnimation = useCallback(() => {
+    const word = words[words.indexOf(currentWord) + 1] || words[0];
+    setCurrentWord(word);
+    setIsAnimating(true);
+  }, [currentWord, words]);
+
+  useEffect(() => {
+    if (!isAnimating)
+      setTimeout(() => {
+        startAnimation();
+      }, duration);
+  }, [isAnimating, duration, startAnimation]);
+
+  return (
+    <AnimatePresence
+      onExitComplete={() => {
+        setIsAnimating(false);
+      }}
+    >
+      <motion.span
+        initial={{
+          opacity: 0,
+        }}
+        animate={{
+          opacity: 1,
+        }}
+        transition={{
+          duration: 0.4,
+          ease: "easeInOut",
+          stiffness: 100,
+          damping: 10,
+        }}
+        exit={{
+          opacity: 0,
+          filter: "blur(12px)",
+          scale: 1.5,
+          position: "absolute",
+        }}
+        className={cn(
+          "z-10 inline-block relative text-left",
+          className
+        )}
+        key={currentWord}
+      >
+        {currentWord.split("").map((letter, index) => {
+          const isSpace = letter === " ";
+
+          // ease out the animation
+          const reverseIndex = currentWord.length - index - 1;
+          const opacity = [0.6, 0.7, 0.8, 0.9]
+          const blur = [2, 1.5, 1, 0.5]
+          const blurAmount = blur[reverseIndex] || 0;
+          const opacityAmount = opacity[reverseIndex] || 1;
+
+          return isSpace ? (
+            <span key={currentWord + index} className="inline-block">&nbsp;</span>
+          ) : (
+            <motion.span
+              key={currentWord + index}
+              initial={{
+                opacity: 0,
+                y: 10,
+                filter: "blur(8px)"
+              }}
+              animate={{
+                opacity: opacityAmount,
+                y: 0,
+                filter: `blur(${blurAmount}px)`
+              }}
+              transition={{
+                delay: index * 0.05,
+                duration: 0.3,
+              }}
+              className="inline-block"
+            >
+              {letter}
+            </motion.span>
+          );
+        })}
+      </motion.span>
+    </AnimatePresence>
+  );
+};
