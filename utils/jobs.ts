3e2ca8b0e1252321530e9c797576f8911d0e5121
Ryan Kwan <klhong124@gmail.com>
2024-12-18 23:05:12 +0000

    work

---
+++ utils/jobs.ts
@@ -0,0 +1,72 @@
+const jobs = [
+    {
+        name: "Kubrick Group",
+        img: '/image/cdp.png',
+        link: "https://cdpfrontend.prod.kubrickgroup.cloud/",
+        year: 2022,
+        desc:
+            "I developed and maintained a e-learning platform for Kubrick Group.",
+        tags: [
+            "Storybook",
+            "JavaScript",
+            "Design",
+            "Vue / Nuxt.js",
+            "React / Next.js",
+            "Apollo GraphQL",
+            "Figma",
+            "GSAP",
+            "Tailwind",
+        ],
+    },
+    {
+        name: "Gymism Club",
+        img: "https://ryankwan.netlify.app/img/gymism.png",
+        link: "https://beta.gymism.club/",
+        year: 2021,
+        desc:
+            "It is a coach matching platform. It includes information about personal fitness trainers and promotes a wide range of sports.",
+        tags: [
+            "Nuxt",
+            "Vue",
+            "Firebase",
+            "PWA",
+            "ApolloGrahpQL",
+            "Stripe",
+            "Cloud Run",
+        ],
+    },
+    {
+        name: "Car8",
+        img: "https://ryankwan.netlify.app/img/car8.png",
+        link: "https://www.car8.com/",
+        year: 2020,
+        desc: "A used-car trading platform made of Vue and Laravel.",
+        tags: [
+            "Laravel",
+            "Vue",
+            "UBA",
+            "Stripe Payment",
+            "Vuex",
+            "SQL",
+            "Python OCR",
+        ],
+    },
+    {
+        name: "IPCC",
+        img: "https://ryankwan.netlify.app/img/ipcc.png",
+        link: "https://www.ipcc.gov.hk/symposium2019/view/index.html",
+        year: 2019,
+        desc: "An event website made of pure HTML and CSS.",
+        tags: ["HTML", "CSS", "Graphic Design", "AOS Animation"],
+    },
+    {
+        name: "Facebook Add Friends Bot",
+        img: "https://ryankwan.netlify.app/img/facebook.png",
+        year: 2018,
+        desc:
+            "An application that automatically login to Facebook and sends friend requests to the others. With a function to remove all friend requests as well.",
+        tags: ["Java", "Selenium", "Web Scraping"],
+    },
+]
+
+export default jobs;
