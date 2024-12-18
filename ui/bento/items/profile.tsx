97941680f1b0a7f8abb1f8217c7212fa51f594e6
Ryan Kwan <klhong124@gmail.com>
2024-12-18 14:45:29 +0000

    GithubStats

--- ui/bento/items/profile.tsx
+++ ui/bento/items/profile.tsx
@@ -1,49 +1,86 @@
-"use client";
 import { cn } from "@/utils/cn";
-import Image from "next/image";
 import Dock from "@/ui/dock";
+import getGithubStats from "@/utils/github";
+import { GitBranch, Star, Users } from 'lucide-react'
+import Image from "next/image";
+
+const WakatimeStats = () => {
+    return <a href="https://wakatime.com/@ryankwandev" target="_blank">
+        <img src="https://wakatime.com/badge/user/e5861fa7-60ad-4e2e-8d44-eefbc5ee063e.svg?style=for-the-badge" alt="wakatime" />
+    </a>
+
+}
+
+
+function StatItem({ icon, label, value }: Readonly<{ icon: React.ReactNode, label: string, value: number }>) {
+    return (
+        <div className="flex items-center space-x-4 text-gray-300">
+            {icon}
+            <div>
+                <div className="text-sm font-medium text-muted-foreground">{label}</div>
+                <div className="text-2xl font-bold">{value.toLocaleString()}</div>
+            </div>
+        </div>
+    )
+}
+
+
+
+const GithubStats = async () => {
+    const { stars, repos, followers } = await getGithubStats()
+
+    return (
+        <div className={cn("flex justify-between px-8")}>
+            <StatItem icon={<Star />} label="Stars" value={stars} />
+            <StatItem icon={<GitBranch />} label="Repos" value={repos} />
+            <StatItem icon={<Users />} label="Followers" value={followers} />
+        </div>
+    )
+}
 
 export function Profile() {
+
     return (
         <div className={cn(
-            "p-8"
+            "p-8 flex flex-col gap-2 item"
         )}>
-            {/* profile image */}
-            <div className={cn(
-                "rounded-full w-32 h-32 relative overflow-hidden mb-4"
-            )}>
-                <Image
-                    src="/icon.gif"
-                    alt="Profile"
-                    fill
-                />
-            </div>
 
-            {/* profile name */}
-            <div className="flex items-center gap-4 mb-2">
 
-                <h1 className={cn(
-                    "text-3xl font-bold text-primary"
+            <div className="flex items-center gap-8 mb-4">
+                <div className={cn(
+                    "rounded-full w-32 h-32 relative overflow-hidden"
                 )}>
-                    Ryan Kwan
-                </h1>
+                    <Image
+                        src="/icon.gif"
+                        alt="Profile"
+                        fill
+                    />
+                </div>
 
+
+                <div>
+                    <h1 className={cn(
+                        "text-3xl font-bold text-primary mb-2"
+                    )}>
+                        Ryan Kwan
+                    </h1>
+                    <h2 className={cn(
+                        "text-md font-normal text-secondary"
+                    )}>
+                        Experienced Front-end Developer in Next.js
+                    </h2>
+                    <Dock />
+
+                </div>
 
             </div>
 
+            <GithubStats />
 
             {/* profile title */}
-            <h2 className={cn(
-                "text-md font-normal text-secondary"
-            )}>
-                Experienced Front-end Developer in Next.js
-            </h2>
+            {/* <WakatimeStats /> */}
+            <br />
 
-            <Dock />
-
-            <a href="https://wakatime.com/@ryankwandev" target="_blank">
-                <img src="https://wakatime.com/badge/user/e5861fa7-60ad-4e2e-8d44-eefbc5ee063e.svg?style=for-the-badge" alt="wakatime" />
-            </a>
         </div>
     );
 }
---
+++ utils/github.ts
@@ -0,0 +1,24 @@
+import { Octokit } from '@octokit/rest'
+
+const GITHUB_USERNAME = 'klhong124'
+const GITHUB_REPO = 'Portfolio'
+
+const octokit = new Octokit({
+    auth: process.env.GITHUB_TOKEN
+})
+
+export default async () => {
+    const { data: reposData } = await octokit.request('GET /users/{username}/repos', {
+        username: GITHUB_USERNAME
+    })
+
+    const { data: followersData } = await octokit.request('GET /users/{username}/followers', {
+        username: GITHUB_USERNAME
+    })
+
+    const stars:number = reposData.map(repo => repo.stargazers_count ?? 0).reduce((acc: number, curr: number) => acc + curr, 0)
+    const repos:number = reposData.length
+    const followers:number = followersData.length
+
+    return { stars, repos, followers }
+}
